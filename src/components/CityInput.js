c6ec43b1f2bd1ae2357971ef08bfe52095dbe80d
shubharajgs7 <shubharajgs7@gmail.com>
2023-05-20 21:41:42 +0530

    weather app

--- src/components/CityInput.js
+++ src/components/CityInput.js
@@ -1,64 +1,38 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core';
-import TextField from '@mui/material/TextField';
-
-const useStyles = makeStyles({
-  input: {
-    top: (props) => (props.city ? '-380px' : '-20px'),
-    width: '600px',
-    display: 'inline-block',
-    padding: '10px 0px 10px 30px',
-    lineHeight: '120%',
-    position: 'relative',
-    borderRadius: '20px',
-    outline: 'none',
-    fontSize: '20px',
-    transition: 'all 0.5s ease-out',
-  },
-  loading: {
-    background: '#ffffff url("../images/loading.gif") 97% center no-repeat',
-    backgroundSize: '25px 25px',
-  },
-});
+import React, { useState } from "react";
 
 const CityInput = (props) => {
-  const [city, setCity] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-  const classes = useStyles(props);
 
-  const onKeyPressHandler = async (e) => {
+  const onClickHandler = async (e) => {
     e.persist();
     const eventKey = e.which ? e.which : e.keyCode;
-    const cityName = e.target.value;
+    const city = e.target.value;
 
-  {
-      if (/^[a-zA-ZäöüÄÖÜß ]+$/.test(cityName)) {
+    // check if input contains only letters after Enter was pressed
+    if (eventKey === 13) {
+      if (/^[a-zA-ZäöüÄÖÜß ]+$/.test(city)) {
         setIsLoading(true);
 
-        if (await props.makeApiCall(cityName)) {
-          e.target.placeholder = 'Enter a City...';
+        if (await props.makeApiCall(city)) {
+          e.target.placeholder = "Enter a City...";
         } else {
-          e.target.placeholder = 'City was not found, try again...';
+          e.target.placeholder = "City was not found, try again...";
         }
       } else {
-        e.target.placeholder = 'Please enter a valid city name...';
+        e.target.placeholder = "Please enter a valid city name...";
       }
 
       setIsLoading(false);
-      e.target.value = '';
+      e.target.value = "";
     }
   };
 
   return (
-    
-    <TextField className={`${classes.input} ${isLoading ? classes.loading : ''}`} label="Enter a City..." variant="outlined" />
-    // <input
-    //   className={`${classes.input} ${isLoading ? classes.loading : ''}`}
-    //   type='text'
-    //   placeholder='Enter a City...'
-    //   onKeyPress={onKeyPressHandler}
-    // />
-
+    <input
+      type="text"
+      placeholder="Enter a City..."
+      onKeyPress={onClickHandler}
+    />
   );
 };
 
--- src/components/MainWeatherWindow.js
+++ src/components/MainWeatherWindow.js
@@ -1,44 +1,33 @@
-import React from 'react';
-import './MainWeatherWindow.css';
+import React from "react";
 
 const MainWeatherWindow = (props) => {
-  const Title = props.city ? null : <h1 className='title'>Weather Forecast</h1>;
+  const Title = props.city ? null : <h1 className="title">Weather Forecast</h1>;
 
   return (
-    <div className='main'>
-      <div className='inner-main'>
+    <div>
+      <div>
         {Title}
         <img
           src={
             props.data
               ? require(`../images/${props.data.icon}.svg`)
-              : require('../images/01d.svg')
+              : require("../images/01d.svg")
           }
-          alt='sun'
-          style={{
-            visibility: props.city ? 'visible' : 'hidden',
-            opacity: props.city ? '1' : '0'
-          }}
+          alt="sun"
         />
-        <div
-          className='today'
-          style={{
-            visibility: props.city ? 'visible' : 'hidden',
-            opacity: props.city ? '1' : '0'
-          }}
-        >
+        <div className="today">
           <span>Today</span>
           <h1>{props.city}</h1>
           <p>
             Temperature: {props.data ? Math.round(props.data.temp - 273.15) : 0}
             °C
           </p>
-          <p>{props.data ? props.data.weather_desc.toLowerCase() : ''}</p>
+          <p>{props.data ? props.data.weather_desc.toLowerCase() : ""}</p>
         </div>
       </div>
       {props.children}
     </div>
   );
-}
+};
 
 export default MainWeatherWindow;
