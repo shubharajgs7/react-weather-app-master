10c89ae912bccfa2d8555e286fcaa7d014130f2b
shubharajgs7 <shubharajgs7@gmail.com>
2023-05-23 17:53:37 +0530

    weather app

--- src/components/CityInput.js
+++ src/components/CityInput.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import TextField from "@mui/material/TextField";
 import { makeStyles } from "@material-ui/core";
 const useStyles = makeStyles({
